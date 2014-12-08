51958891231b1c7a0b5744a75f9d826698a8bccf
ChangKH <web@kunhsiang.com>
2014-12-08 10:51:42 +0800

    Age Of Laika Complete!

--- Age of Laika/ViewController.swift
+++ Age of Laika/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    @IBOutlet weak var enterHumanYearsTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func convertToDogYearsButtonPressed(sender: UIButton) {
+        let stringFromTextField = enterHumanYearsTextField.text
+        
+        let optionalIntFromTextField = stringFromTextField.toInt()
+        
+        let intFromOptional = optionalIntFromTextField!
+        
+        dogYearsLabel.text = "\(intFromOptional*7) Human Years"
+        dogYearsLabel.hidden = false
+        enterHumanYearsTextField.resignFirstResponder()
+    }
 }
-
